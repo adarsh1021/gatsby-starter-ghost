bad3de6b7ec144fd3a6017919727371baae49a60
Adarsh Menon <adarsh1021@gmail.com>
2020-08-28 16:35:25 +0530

    url conf

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `sad-johnson-dad5f6.netlify.app`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://sad-johnson-dad5f6.netlify.app/`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
