a871471da597fcc9aea8b047a0cf2a8ad68c0969
Adarsh Menon <adarsh1021@gmail.com>
2020-10-08 09:23:12 +0530

    updates

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -1,27 +1,31 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Helmet } from 'react-helmet'
-import { Link, StaticQuery, graphql } from 'gatsby'
-import Img from 'gatsby-image'
+import React from "react";
+import PropTypes from "prop-types";
+import { Helmet } from "react-helmet";
+import { Link, StaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
 
-import { Navigation } from '.'
-import config from '../../utils/siteConfig'
+import { Navigation } from ".";
+import config from "../../utils/siteConfig";
 
 // Styles
-import '../../styles/app.css'
+import "../../styles/app.css";
 
 /**
-* Main layout component
-*
-* The Layout component wraps around each page and template.
-* It also provides the header, footer as well as the main
-* styles, and meta data for each page.
-*
-*/
+ * Main layout component
+ *
+ * The Layout component wraps around each page and template.
+ * It also provides the header, footer as well as the main
+ * styles, and meta data for each page.
+ *
+ */
 const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
-    const site = data.allGhostSettings.edges[0].node
-    const twitterUrl = site.twitter ? `https://twitter.com/${site.twitter.replace(/^@/, ``)}` : null
-    const facebookUrl = site.facebook ? `https://www.facebook.com/${site.facebook.replace(/^\//, ``)}` : null
+    const site = data.allGhostSettings.edges[0].node;
+    const twitterUrl = site.twitter
+        ? `https://twitter.com/${site.twitter.replace(/^@/, ``)}`
+        : null;
+    const facebookUrl = site.facebook
+        ? `https://www.facebook.com/${site.facebook.replace(/^\//, ``)}`
+        : null;
 
     return (
         <>
@@ -32,10 +36,62 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
             </Helmet>
 
             <div className="viewport">
-
                 <div className="viewport-top">
+                    <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-pink-500 mb-3">
+                        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+                            <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+                                <a
+                                    className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white"
+                                    href="#pablo"
+                                >
+                                    pink Tailwind Starter Kit
+                                </a>
+                                <button
+                                    className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                                    type="button"
+                                >
+                                    <i className="fas fa-bars"></i>
+                                </button>
+                            </div>
+                            <div
+                                className={"lg:flex flex-grow items-center"}
+                                id="example-navbar-danger"
+                            >
+                                <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+                                    <li className="nav-item">
+                                        <a
+                                            className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                                            href="#pablo"
+                                        >
+                                            <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i>
+                                            <span className="ml-2">Share</span>
+                                        </a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a
+                                            className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                                            href="#pablo"
+                                        >
+                                            <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i>
+                                            <span className="ml-2">Tweet</span>
+                                        </a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a
+                                            className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                                            href="#pablo"
+                                        >
+                                            <i className="fab fa-pinterest text-lg leading-lg text-white opacity-75"></i>
+                                            <span className="ml-2">Pin</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+
                     {/* The main header section on top of the screen */}
-                    <header className="site-head" style={{ ...site.cover_image && { backgroundImage: `url(${site.cover_image})` } }}>
+                    {/* <header className="site-head" style={{ ...site.cover_image && { backgroundImage: `url(${site.cover_image})` } }}>
                         <div className="container">
                             <div className="site-mast">
                                 <div className="site-mast-left">
@@ -57,24 +113,28 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                                     <h1 className="site-banner-title">{site.title}</h1>
                                     <p className="site-banner-desc">{site.description}</p>
                                 </div> :
-                                null}
-                            <nav className="site-nav">
-                                <div className="site-nav-left">
-                                    {/* The navigation items as setup in Ghost */}
-                                    <Navigation data={site.navigation} navClass="site-nav-item" />
-                                </div>
-                                <div className="site-nav-right">
-                                    <Link className="site-nav-button" to="/about">About</Link>
-                                </div>
-                            </nav>
+                                null} */}
+                    {/* <nav className="site-nav">
+                        <div className="site-nav-left">
+                            The navigation items as setup in Ghost
+                            <Navigation
+                                data={site.navigation}
+                                navClass="site-nav-item"
+                            />
                         </div>
-                    </header>
+                        <div className="site-nav-right">
+                            <Link className="site-nav-button" to="/about">
+                                About
+                            </Link>
+                        </div>
+                    </nav> */}
+                    {/* </div>
+                    </header> */}
 
                     <main className="site-main">
                         {/* All the main content gets inserted here, index.js, post.js */}
                         {children}
                     </main>
-
                 </div>
 
                 <div className="viewport-bottom">
@@ -82,20 +142,30 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                     <footer className="site-foot">
                         <div className="site-foot-nav container">
                             <div className="site-foot-nav-left">
-                                <Link to="/">{site.title}</Link> © 2019 &mdash; Published with <a className="site-foot-nav-item" href="https://ghost.org" target="_blank" rel="noopener noreferrer">Ghost</a>
+                                <Link to="/">{site.title}</Link> © 2019 &mdash;
+                                Published with{" "}
+                                <a
+                                    className="site-foot-nav-item"
+                                    href="https://ghost.org"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Ghost
+                                </a>
                             </div>
                             <div className="site-foot-nav-right">
-                                <Navigation data={site.navigation} navClass="site-foot-nav-item" />
+                                <Navigation
+                                    data={site.navigation}
+                                    navClass="site-foot-nav-item"
+                                />
                             </div>
                         </div>
                     </footer>
-
                 </div>
             </div>
-
         </>
-    )
-}
+    );
+};
 
 DefaultLayout.propTypes = {
     children: PropTypes.node.isRequired,
@@ -105,9 +175,9 @@ DefaultLayout.propTypes = {
         file: PropTypes.object,
         allGhostSettings: PropTypes.object.isRequired,
     }).isRequired,
-}
+};
 
-const DefaultLayoutSettingsQuery = props => (
+const DefaultLayoutSettingsQuery = (props) => (
     <StaticQuery
         query={graphql`
             query GhostSettings {
@@ -118,7 +188,7 @@ const DefaultLayoutSettingsQuery = props => (
                         }
                     }
                 }
-                file(relativePath: {eq: "ghost-icon.png"}) {
+                file(relativePath: { eq: "ghost-icon.png" }) {
                     childImageSharp {
                         fixed(width: 30, height: 30) {
                             ...GatsbyImageSharpFixed
@@ -127,8 +197,8 @@ const DefaultLayoutSettingsQuery = props => (
                 }
             }
         `}
-        render={data => <DefaultLayout data={data} {...props} />}
+        render={(data) => <DefaultLayout data={data} {...props} />}
     />
-)
+);
 
-export default DefaultLayoutSettingsQuery
+export default DefaultLayoutSettingsQuery;
---
+++ src/pages/adarsh/testing.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Layout } from "../../components/common";
+
+const AdarshTest = () => (
+    <Layout>
+        <div className="container">
+            <article className="content" style={{ textAlign: `center` }}>
+                <h1 className="content-title">Welcome Adarsh</h1>
+            </article>
+        </div>
+    </Layout>
+);
+
+export default AdarshTest;
